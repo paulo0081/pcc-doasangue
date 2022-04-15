ecb6bdabf88c4621054fb60be6ae2e27506dad13
gabrielandm <gabriel.paoli@kumulus.com.br>
2022-04-15 11:24:25 -0300

    Template para aplicativo React Native criado

---
+++ front/DoaSangue/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Mousse UwU</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
